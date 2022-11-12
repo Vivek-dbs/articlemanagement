4777afd061b507633c6d2c2029a410466af8840f
Vivek-dbs <108936199+Vivek-dbs@users.noreply.github.com>
2022-11-12 11:56:57 +0530

    Add files via upload

--- src/app/user/user-routing.module.ts
+++ src/app/user/user-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { BookDetailsComponent } from './book-details/book-details.component';
+import { ArticleDetailsComponent } from './article-details/article-details.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
   {path: 'dashboard',component: DashboardComponent},
-  {path: 'article-details/:id',component: BookDetailsComponent},
+  {path: 'article-details/:id',component: ArticleDetailsComponent},
   {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
   {path: '**', redirectTo: 'dashboard', pathMatch: 'full'},
 ];
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -4,14 +4,14 @@ import { CommonModule } from '@angular/common';
 import { UserRoutingModule } from './user-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { CarouselModule } from 'ngx-owl-carousel-o';
-import { BookDetailsComponent } from './book-details/book-details.component';
+import { ArticleDetailsComponent } from './article-details/article-details.component';
 import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
     DashboardComponent,
-    BookDetailsComponent
+    ArticleDetailsComponent
   ],
   imports: [
     CommonModule,
